6f519d1415ff9a11f8dbde38f785808ceec7bfbf
Muhammed Mustafa <muuhamed14mustafa@gmail.com>
2023-05-04 20:34:33 +0300

    Add connecting to database
    - install mongoose to connect to database
    - install colors and morgan in dependencies
    - install cross-env in devDependencies

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
+import colors from 'colors';
+import { connectDB } from './config/db.js';
 import { mountRoutes } from './routes/index.js';
 
 // Load env vars
 dotenv.config({ path: './src/config/config.env' });
+
+// Connect to database
+connectDB();
 
 const app = express();
 
@@ -17,7 +22,17 @@ mountRoutes(app);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server Running In ${process.env.NODE_ENV} Mode On Port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close server & exit process
+
+  server.close(() => process.exit(1));
+});
