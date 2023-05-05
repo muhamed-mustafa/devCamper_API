48e175cf29bba28c108424340b7e7559be6b7032
Muhammed Mustafa <muuhamed14mustafa@gmail.com>
2023-05-05 19:06:43 +0300

    Add Course routes & getCourses

--- src/routes/bootcamp.js
+++ src/routes/bootcamp.js
@@ -7,8 +7,12 @@ import {
   deleteBootCamp,
   getBootcampsInRadius,
 } from '../controllers/bootcamp.js';
+import { courseRoute } from './course.js';
 
 const router = Router();
+
+// Re-route into other resource routers
+router.use('/:bootcampId/courses', courseRoute);
 
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 router.route('/').get(getBootCamps).post(createBootCamp);
